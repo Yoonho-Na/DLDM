7cd69f62886a1187b669adcfddf8c1838e0c7f01
Yoonho Na <yoonho94.na@gmail.com>
2024-01-18 12:24:41 +0900

    Update pretrained_dldm.py

--- DLDM/scripts/pretrained_dldm.py
+++ DLDM/scripts/pretrained_dldm.py
@@ -3,9 +3,9 @@ import gdown, os, tarfile
 pretrained_folder = 'pretrained_DLDM'
 os.makedirs(pretrained_folder, exist_ok=True)
 google_path = 'https://drive.google.com/uc?id='
-file_id = '1JriniwO28vw_eRtN08gvHuc_EM6PlLDc'
+file_id = '1xsQibTHvqoiXg2na1UfjMau9ZPMze8Sx'
 output_name = pretrained_folder+'/DLDM_pretrained.tar.gz'
-# gdown.download(google_path+file_id,output_name,quiet=False)
+gdown.download(google_path+file_id,output_name,quiet=False)
 file = tarfile.open(output_name)
 file.extractall(pretrained_folder)
 file.close()
